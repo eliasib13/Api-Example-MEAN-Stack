5a659296d93674103a171306cf7dca2010c1505d
Eliasib García <eliasib.lv.12@gmail.com>
2017-03-23 13:36:08 +0000

    Added response headers

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, content-Type, Accept, Access-Control-Request-Method');
+    res.header('Access-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE');
+    res.header('Allow', 'GET, POST, PUT, OPTIONS, DELETE');
+
+    next();
+});
+
 app.use('/api', api);
 
 module.exports = app;
