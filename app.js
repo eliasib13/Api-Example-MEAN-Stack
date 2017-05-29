a3e64d560d5786974d2ccc46f8204d72da27b9ba
Eliasib García <eliasib.lv.12@gmail.com>
2017-05-29 14:26:18 +0100

    Fixed error on headers.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, content-Type, Accept, Access-Control-Request-Method');
-    res.header('Access-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE');
     res.header('Allow', 'GET, POST, PUT, OPTIONS, DELETE');
 
     next();
